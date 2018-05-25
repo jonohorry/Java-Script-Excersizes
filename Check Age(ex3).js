79e416a173aaa1af4727017379012c8c41ed7daa
jonohorry <39629456+jonohorry@users.noreply.github.com>
2018-05-25 11:52:07 -0500

    Create Check Age(ex3).js

---
+++ Check Age(ex3).js
@@ -0,0 +1,13 @@
+//returns age 
+function checkAge(name, age) {
+  // your code here
+  if (age >=21){
+    return("Welcome, " + name + "!");
+  }
+  if (age < 21)
+  {
+    return("Go home, " + name + "!");
+  }
+}
+var output = checkAge('Adrian', 22);
+console.log(output); // --> 'Welcome, Adrian!'
