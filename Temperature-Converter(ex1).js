734aa170af754f636402ac7135586d3e74b0a7ae
jonohorry <39629456+jonohorry@users.noreply.github.com>
2018-05-25 11:46:37 -0500

    Create Temperature-Converter(ex1).js

---
+++ Temperature-Converter(ex1).js
@@ -0,0 +1,22 @@
+//temperature converter
+function cToF(celsius) {
+  // this is celsius to Fahrenheit
+  
+  var cTemp = celsius; 
+  CtF = cTemp * 1.8 + 32;
+  var message = cTemp+'\xB0C is ' + CtF + '\xB0F.';
+  console.log(message);
+}
+
+function fToC(fahrenheit) {
+  // this is Fahrenheit to Celsius
+  var fTemp = fahrenheit; 
+  FtC = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + FtC + '\xB0C.';
+  console.log(message);
+  
+  
+}
+
+cToF(60);
+fToC(45);
